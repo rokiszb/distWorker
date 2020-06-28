6b13484f663ea0a692f042520316aec488f2a598
rokiszb <rokas.lakstauskas@gmail.com>
2020-06-28 14:09:44 +0300

    code cleanup

--- src/Controller/ProcessController.php
+++ src/Controller/ProcessController.php
@@ -9,34 +9,16 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpClient\CurlHttpClient;
-use Symfony\Component\HttpClient\Exception\TransportException;
 use Symfony\Component\HttpClient\Exception\InvalidArgumentException;
 
 class ProcessController extends AbstractController
 {
-    const MAX_JOBS = 4;
-    /**
-     * @Route("/process", name="process")
-     */
-    public function initiateRequestCodeFetch(JobRepository $jobRepository, ProcessHelper $processHelper, EntityManagerInterface $em)
-    {
-        $job = null;
-        $response = new JsonResponse();
-
-        if (!empty($job)) {
-            $status = $processHelper->checkHttpResponseCode($job, $em);
-            $response->setData(['status' => $status]);
-        } else {
-            $response->setData(['status' => 'Not available']);
-        }
-
-        return $response;
-    }
+    const MAX_JOBS = 10;
 
     /**
      * @Route("/start_jobs", name="start_jobs")
      */
-    public function initiateMultipleJobRequests(JobRepository $jobRepository, ProcessHelper $processHelper, EntityManagerInterface $em)
+    public function initiateMultipleJobRequests(JobRepository $jobRepository, EntityManagerInterface $em, ProcessHelper $processHelper)
     {
         $client = new CurlHttpClient();
         
@@ -53,6 +35,7 @@ class ProcessController extends AbstractController
             try {
                 $responses[$job->getId()] = $client->request('GET', $job->getUrl());
             } catch (InvalidArgumentException $exception) {
+                // caught exception could be logged later on
                 $responses[$job->getId()] = null;
             } 
 
@@ -61,37 +44,10 @@ class ProcessController extends AbstractController
             $jobCount++;
         }
         
-        foreach ($responses as $rKey => $response) {
-            $job = $jobArray[$rKey];
-            if (is_null($response)) {
-                $job->setStatus('ERROR');
-            } else {
-                try {
-                    $job->setHttpCode($response->getStatusCode());
-                    $job->setStatus('DONE');
-                } catch (TransportException $transportException) {
-                    $job->setStatus('ERROR');
-                } 
-            }
-        }
-        $em->flush();
-                
+        $jobArray = $processHelper->updateJobHttpCodes($responses, $jobArray);
+
+        if ($jobCount > 0) $em->flush();
+
         return new JsonResponse(json_encode(['status' => 'ok']));
     }
 }
-
-// $job->setStatus('PROCESSING');
-// $em->flush();
-// $client = HttpClient::create();
-
-// try {
-//     $response = $client->request('GET', $job->getUrl());
-//     $status = ['code' => $response->getStatusCode(), 'status' => 'DONE'];
-// } catch (InvalidArgumentException $exception) {
-//     $status = ['code' => null, 'status' => 'ERROR'];
-// } catch (TransportException $transportException) {
-//     $status = ['code' => null, 'status' => 'ERROR'];
-// } 
-
-// $job->setHttpCode($status['code'])->setStatus($status['status']);
-// $em->flush();
--- src/Service/ProcessHelper.php
+++ src/Service/ProcessHelper.php
@@ -1,31 +1,25 @@
 <?php
 
 namespace App\Service;
-use Symfony\Component\HttpClient\HttpClient;
-use App\Entity\Job;
-use Doctrine\ORM\EntityManager;
-use InvalidArgumentException;
 use Symfony\Component\HttpClient\Exception\TransportException;
 
 class ProcessHelper
 {
-    public function checkHttpResponseCode(Job $job, $em) {
-        $job->setStatus('PROCESSING');
-        $em->flush();
-        $client = HttpClient::create();
-
-        try {
-            $response = $client->request('GET', $job->getUrl());
-            $status = ['code' => $response->getStatusCode(), 'status' => 'DONE'];
-        } catch (InvalidArgumentException $exception) {
-            $status = ['code' => null, 'status' => 'ERROR'];
-        } catch (TransportException $transportException) {
-            $status = ['code' => null, 'status' => 'ERROR'];
-        } 
-
-        $job->setHttpCode($status['code'])->setStatus($status['status']);
-        $em->flush();
-
-        return $status['status'];
+    public function updateJobHttpCodes(array $responses, array $jobArray)
+    {
+        foreach ($responses as $rKey => $response) {
+            $job = $jobArray[$rKey];
+            if (is_null($response)) {
+                $job->setStatus('ERROR');
+            } else {
+                try {
+                    $job->setHttpCode($response->getStatusCode());
+                    $job->setStatus('DONE');
+                } catch (TransportException $transportException) {
+                    $job->setStatus('ERROR');
+                } 
+            }
+        }
+        return $jobArray;
     }
 }
